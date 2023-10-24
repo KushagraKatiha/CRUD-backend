0fdc9d521bc95a206cf42a925564e227ae021f56
kushagrakatiha <kushagrakatiha123@gmail.com>
2023-10-24 06:48:24 +0530

    delete user created

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const { home, addUser, getUser } = require('../controllers/userController');
+const { home, addUser, getUser, delUser } = require('../controllers/userController');
 
 const router = express.Router();
 
 router.get('/', home);
 router.post('/adduser', addUser)
 router.post('/getuser', getUser)
-
-
+router.delete('/deleteuser', delUser)
 
 module.exports = router;
